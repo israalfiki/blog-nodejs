817dd96883c2e634192225adcc341365a744b226
israalfiki <35269342+israalfiki@users.noreply.github.com>
2021-08-16 12:07:06 +0200

    added post controller

---
+++ controllers/post.js
@@ -0,0 +1,95 @@
+const Post = require('../models/post')
+
+// get all posts
+exports.getPosts = (req, res) => {
+        Post.find()
+          .then((blogs) => {
+            res.render("../views/index.ejs", {
+              title: "My Great Fucking Blog",
+              blogs: blogs,
+            });
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+}
+
+//create post
+exports.showCreatePost = (req, res) => {
+    res.render("../views/create-post.ejs", { title: "Create New Post" });
+  }
+
+exports.createPost = (req, res) => {
+    console.log(req.body);
+    const post = new Post({
+      title: req.body.title,
+      snippet: req.body.snippet,
+      body: req.body.body,
+    });
+    post
+      .save()
+      .then((result) => {
+        res.redirect("/");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+//show a single post
+exports.showPost = (req, res) => {
+    const id = req.params.id;
+    Post.findById(id)
+      .then((post) => {
+        res.render("../views/show-post.ejs", { title: post.title, post: post });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+//update a single post
+exports.showEditPost = (req, res) => {
+    console.log("edit mode");
+    const id = req.params.id;
+    Post.findById(id)
+      .then((post) => {
+        res.render("../views/edit-post.ejs", {
+          title: "Edit a post",
+          post: post,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+}
+
+exports.updatePost = (req, res) => {
+    const id = req.params.id;
+    const title = req.body.title;
+    const snippet = req.body.snippet;
+    const body = req.body.body;
+    Post.findByIdAndUpdate(id, {
+      title: title,
+      snippet: snippet,
+      body: body,
+    }).then((result)=>{
+        console.log('Updated Successfuly!')
+        res.redirect('/')
+    }).catch(err=>{
+        console.log(err)
+    })
+}
+
+//delete a single post
+exports.deletePost = (req, res) => {
+    const id = req.params.id;
+    Post.findByIdAndDelete(id)
+      .then((result) => {
+        console.log("record deleted!");
+        res.redirect("/");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
--- routes/routes.js
+++ routes/routes.js
@@ -1,105 +1,30 @@
 const express = require("express");
 
-const Post = require("../models/posts");
+const Post = require("../models/post");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  Post.find()
-    .then((blogs) => {
-      res.render("../views/index.ejs", {
-        title: "My Great Fucking Blog",
-        blogs: blogs,
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+const postController = require('../controllers/post')
+
+// index page
+router.get("/", postController.getPosts );
 
 //create post
-router.get("/blogs/create", (req, res) => {
-  res.render("../views/create-post.ejs", { title: "Create New Post" });
-});
+router.get("/blogs/create", postController.showCreatePost);
 
-router.post("/blogs/add-post", (req, res) => {
-  console.log(req.body);
-  const post = new Post({
-    title: req.body.title,
-    snippet: req.body.snippet,
-    body: req.body.body,
-  });
-  post
-    .save()
-    .then((result) => {
-      res.redirect("/");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+router.post("/blogs/add-post", postController.createPost);
 
 // view a single post
 
-router.get("/posts/:id", (req, res) => {
-  const id = req.params.id;
-  Post.findById(id)
-    .then((post) => {
-      res.render("../views/show-post.ejs", { title: post.title, post: post });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+router.get("/posts/:id", postController.showPost);
 
 //update a single post
-router.get("/posts/:id/edit", (req, res) => {
-  console.log("edit mode");
-  const id = req.params.id;
-  Post.findById(id)
-    .then((post) => {
-      const title = post.title;
-      const snippet = post.snippet;
-      const body = post.body;
-      res.render("../views/edit-post.ejs", {
-        title: "Edit a post",
-        post: post,
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+router.get("/posts/:id/edit", postController.showEditPost);
 
-router.post("/posts/:id/update", (req, res) => {
-  const id = req.params.id;
-  const title = req.body.title;
-  const snippet = req.body.snippet;
-  const body = req.body.body;
-  Post.findByIdAndUpdate(id, {
-    title: title,
-    snippet: snippet,
-    body: body,
-  }).then((result)=>{
-      console.log('Updated Successfuly!')
-      res.redirect('/')
-  }).catch(err=>{
-      console.log(err)
-  })
-});
+router.post("/posts/:id/update", postController.updatePost);
 
 //delete a single post
-router.post("/posts/:id/delete", (req, res) => {
-  const id = req.params.id;
-  Post.findByIdAndDelete(id)
-    .then((result) => {
-      console.log("record deleted!");
-      res.redirect("/");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+router.post("/posts/:id/delete", postController.deletePost);
 
 // about and 404 page
 router.get("/about", (req, res) => {
